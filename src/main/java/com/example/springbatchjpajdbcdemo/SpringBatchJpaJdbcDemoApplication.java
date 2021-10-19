5c22ead5bf39adf7a4be35af86b547149efe8037
Vamshi Epuri <a167250@pm.local>
2021-10-19 14:16:41 -0500

    init commit

---
+++ src/main/java/com/example/springbatchjpajdbcdemo/SpringBatchJpaJdbcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchjpajdbcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchJpaJdbcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchJpaJdbcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbatchjpajdbcdemo/SpringBatchJpaJdbcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchjpajdbcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchJpaJdbcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
